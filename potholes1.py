6bb2851449082a78963d87138313d8fe780ad2d7
Shabeer Mohammad <46803563+ShabeerMohammad@users.noreply.github.com>
2021-05-20 21:37:21 +0530

    potholes2

--- potholes1.py
+++ potholes1.py
@@ -1,67 +1,86 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array,load_img
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
 import numpy as np
 import cv2
+from imutils import paths
+import os
+from shutil import rmtree
 import glob
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from imutils.object_detection import non_max_suppression
-from nms_user import non_max_suppression_slow
-from helpers import sliding_window
-import matplotlib.pyplot as plt
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from sklearn.metrics import confusion_matrix,classification_report
 
-potholes_model = load_model('/home/smohammad/Projects/Raj_Project/Pothole_New/model/pothole_model_224.h5')
+potholes_model = load_model('potholes_model.h5')
 
 #InputImages = list(paths.list_images('./Images/'))
-path = "/home/smohammad/Projects/Raj_Project/Pothole_New/inputs/IMG_20201115_070153.jpg"
+path = "./Input/*.*"
+classes2 = []
+for num,InputImage in enumerate (glob.glob(path)):
 
-#for num,InputImage in enumerate (glob.glob(path)):
-
-image = load_img(path,target_size=(224,224))
-image = img_to_array(image)
-image = preprocess_input(image)
-image= np.expand_dims(image,axis=0)
-result = potholes_model.predict(image)
-
-if result[0][0]>0.65:
-    print('Pothole')    
-
+    img_ph = cv2.imread(InputImage,cv2.IMREAD_COLOR)
+    img_ph2 = cv2.resize(img_ph,(224,224))
+    img_ph_arr = img_to_array(img_ph2)
+    img_ph_prep  = preprocess_input(img_ph_arr)
+    img_ph_res = np.reshape(img_ph_prep,[1,224,224,3])
     
-else:
-    print('Normal')
+    classes1 = potholes_model.predict(img_ph_res)
+    classes2.append(classes1)
+predicted_classes = np.argmax(classes2, axis=1)
+#print(classes2)
+    #classes1 = classes1.tolist()
+    
+        #print(classes1)
+    flat_list = []
+    for sublist in classes1:
+        for item in sublist:
+            flat_list.append(item)
 
+    labels = ['Not a Pothole','Pothole']
+    zipped_list = list(list(x) for x in zip(labels,flat_list))
+    res = sorted(zipped_list,key = lambda x: x[1])
+    text = str(res[-1][0])
+        #print(text)
 
-plt.figure(figsize=(10,10))
-plt.imshow(image)
-plt.title('processed')
+    font = cv2.FONT_HERSHEY_COMPLEX
+    bottomLeft = (40,60)
+    fontscale = 1
+    fontcolor = (75,75,75)
+    linetype = 2
 
-plt.show()
+        #type(image)
+    img_2 = cv2.resize(img_ph2,(224,224))
+    cv2.putText(img_2,text,bottomLeft,font,fontscale,fontcolor,linetype)
+    cv2.rectangle(img_2,(1,1),(224,224),(0,255,0),2,2)
+    #cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+        #img_ph2 = cv2.imshow("image with text ",img_ph2)
+    #cv2.imshow("image with text ",img_ph2)
+        #img_v = cv2.vconcat(img_ph,img_ph2)
+        #cv2.imshow("image with input & output ",img_v)
+    img_concate_Hori=np.concatenate((img_ph2,img_2),axis=1)
+        #concatanate image Vertically
+        #img_concate_Verti=np.concatenate((img_ph,img_ph2),axis=0)
+    #cv2.imshow('concatenated_Hori',img_concate_Hori)
+        #cv2.imshow('concatenated_Verti',img_concate_Verti)
+    #cv2.imwrite("./Output/potholes{}.jpg".format(num),img_concate_Hori)
 
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
-#classes2.append(classes1)
-flat_list = []
-for sublist in result:
-    for item in sublist:
-        flat_list.append(item)
+classes1
 
-labels = ['Not a Pothole','Pothole']
-zipped_list = list(list(x) for x in zip(labels,flat_list))
-res = sorted(zipped_list,key = lambda x: x[1])
-text = str(res[-1][0])
-    #print(text)
+#number of inputs
+#number of potholes detected TP
+test_generator = ImageDataGenerator()
+test_data_generator = test_generator.flow_from_directory(
+     './Input/',target_size=(224, 224),batch_size=4,shuffle=False)
+test_steps_per_epoch = np.math.ceil(test_data_generator.samples / test_data_generator.batch_size)
 
-font = cv2.FONT_HERSHEY_COMPLEX
-bottomLeft = (40,60)
-fontscale = 1
-fontcolor = (75,75,75)
-linetype = 2
-
-    #type(image)
-img_2 = cv2.resize(test_image,(64,64))
-cv2.putText(img_2,text,bottomLeft,font,fontscale,fontcolor,linetype)
-cv2.rectangle(img_2,(1,1),(64,64),(0,255,0),2,2)
-img_concate_Hori=np.concatenate((test_image,img_2),axis=1)
-#cv2.imshow('concatenated_Hori',img_concate_Hori)
-cv2.imwrite("/home/smohammad/Projects/Raj_Project/Pothole_New/outputs/potholes{}.jpg".format(num),img_concate_Hori)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+predictions = potholes_model.predict_generator(test_data_generator, steps=test_steps_per_epoch)
+# Get most likely class
+predicted_classes = np.argmax(predictions, axis=1)#
+true_classes = test_data_generator.classes
+class_labels = list(test_data_generator.class_indices.keys())
+report = classification_report(true_classes, predicted_classes, target_names=class_labels)
+report = confusion_matrix(true_classes, predicted_classes, target_names=class_labels)
+print(report)   
